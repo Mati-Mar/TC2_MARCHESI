9161f734245e3c1e089bddfb782beb55afe8b993
Mati-Mar <matias.marchesi03@gmail.com>
2026-05-04 17:20:29 -0300

    Mati-Mar - 04/05/2026
    * Se sube tarea semanal 2 en el directorio de tarea_semanal_2

---
+++ tarea_semanal_2/programas/python/butterworth7_polos_modulo_fase.py
@@ -0,0 +1,13 @@
+import scipy.signal as sig
+from pytc2.sistemas_lineales import tf2sos_analog, analyze_sys 
+
+# Filtro Butterworth de orden 7 normalizado
+z, p, k = sig.buttap(7)
+
+num, den = sig.zpk2tf(z, p, k)
+
+# Conversión a secciones de segundo orden
+this_sos = tf2sos_analog(num, den)
+
+# Análisis (módulo y fase)
+_ = analyze_sys(this_sos)
